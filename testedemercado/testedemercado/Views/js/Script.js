f6a3e6707147457abb598a15f7f5708d4ac14d14
flaviagabanela <flavia_gabanela@hotmail.com>
2016-03-03 22:51:25 -0300

    Login e Lista Layout2

---
+++ testedemercado/testedemercado/Views/js/Script.js
@@ -0,0 +1,53 @@
+﻿function validar() {
+	var txtUsuario, txtSenha, txtResultado, btnVerificar;
+
+	txtUsuario = document.getElementById("txtUsuario").value
+	txtSenha = document.getElementById("txtSenha").value
+	txtResultado = document.getElementById("txtResultado")
+
+	if (txtUsuario != "" && txtSenha != "")
+		window.location = "/Views/Lista.html";
+	else
+		//txtResultado.value = "Senha inválida";
+		alert("Senha inválida");
+
+};
+
+function add() {
+	var txtCod, txtCat, txtNome, txtQtd, txtValor, txtNeg, btnAdd, meuSelect, btnRem;
+
+	txtCod = document.getElementById("txtCod").value;
+	txtCat = document.getElementById("txtCat").value;
+	txtNome = document.getElementById("txtTitulo").value;
+	txtQtd = document.getElementById("txtQta").value;
+	txtValor = document.getElementById("txtPreco").value;
+	txtNeg = document.getElementById("txtNeg").value;
+	meuSelect = document.getElementById("meuSelect");
+
+	var nome = txtNome;
+
+	if (nome.length === 0) {
+		alert("Nome inválido!");
+		return;
+	}
+
+	var opt = document.createElement("option");
+	opt.textContent = nome;
+
+	meuSelect.appendChild(opt);
+};
+
+btnRem.onclick = function () {
+	if (meuSelect.selectedIndex < 0) {
+		alert("Não há item selecionado")
+		return;
+	}
+
+	var optSelecionado = meuSelect.options[meuSelect.selectedIndex];
+
+	meuSelect.removeChild(optSelecionado);
+};
+
+
+
+
